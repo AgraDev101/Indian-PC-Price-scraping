5c52ee6cebc7bd56bc95195189aea013891682dc
Abhay <pym.part@gmail.com>
2020-05-14 13:44:15 +0530

    added my github name

--- Main.py
+++ Main.py
@@ -22,6 +22,7 @@ def md_cpu(a,b):
 def md_ram(a,b):
 	return print(a+b)
 
+
 while True:
     try:
         site = int(input("Enter {} for MD Computer {} for PrimeABGB: ".format("'1'", "'2'")))
@@ -31,145 +32,134 @@ while True:
     else:
         break
 
-if (site == 1):
-	print("MD Computer selected")
+while True:
+	try:
+		comp = int(input("Enter {} for CPU or {} for GPU or{} for RAM price list: ".format("'1'", "'2'", "'3'")))
+	except ValueError:
+		print("Sorry, I didn't understand that.")
+		continue
+	else:
+		break
+
+if (site == 1 and comp == 1):
+	print("MD Computer and CPU price selected")
 	while True:
 		try:
-			comp = int(input("\nEnter {} for CPU {} or for GPU {} for RAM price list: ".format("'1'", "'2'", "'3'")))
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
 		except ValueError:
 			print("Sorry, I didn't understand that.")
 			continue
 		else:
 			break
-	if (comp == 1):
-		print("CPU selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		md_cpu(page_from, page_to)
-	if (comp == 2):
-		print("GPU selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		md_gpu(page_from, page_to)
-	if (comp == 3):
-		print("RAM selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		md_ram(page_from, page_to)
-
-elif (site == 2):
-	print("PrimeABGB selected")
 	while True:
 		try:
-			comp = int(input("\nEnter {} for CPU {} for GPU {} for RAM price list: ".format("'1'", "'2'", "'3'")))
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
 		except ValueError:
 			print("Sorry, I didn't understand that.")
 			continue
 		else:
 			break
-	if (comp == 1):
-		print("CPU selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		prime_cpu(page_from, page_to)
-	if (comp == 2):
-		print("GPU selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		prime_gpu(page_from, page_to)
-	if (comp == 3):
-		print("RAM selected")
-		while True:
-			try:
-				page_from = int(input("\nStarting page(e.g., 1 or 2 etc): "))
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		while True:
-			try:
-				page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
-				page_to += 1
-			except ValueError:
-				print("Sorry, I didn't understand that.")
-				continue
-			else:
-				break
-		prime_ram(page_from, page_to)
+	md_cpu(page_from, page_to)
 
+elif (site == 1 and comp == 2):
+	print("MD Computer and GPU price selected")
+	while True:
+		try:
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	while True:
+		try:
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	md_gpu(page_from, page_to)
+
+elif (site == 1 and comp == 3):
+	print("MD Computer and RAM price selected")
+	while True:
+		try:
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	while True:
+		try:
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	md_ram(page_from, page_to)
+
+elif (site == 2 and comp == 1):
+	print("PrimeABGB and CPU price selected")
+	while True:
+		try:
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	while True:
+		try:
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	prime_cpu(page_from, page_to)
+
+elif (site == 2 and comp == 2):
+	print("PrimeABGB and GPU price selected")
+	while True:
+		try:
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	while True:
+		try:
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	prime_gpu(page_from, page_to)
+
+elif (site == 2 and comp == 3):
+	print("PrimeABGB and RAM price selected")
+	while True:
+		try:
+			page_from = int(input("Starting page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	while True:
+		try:
+			page_to = int(input("Upto page(e.g., 1 or 2 etc): "))
+		except ValueError:
+			print("Sorry, I didn't understand that.")
+			continue
+		else:
+			break
+	prime_ram(page_from, page_to)
+
+else:
+	print("Nothing selected")
